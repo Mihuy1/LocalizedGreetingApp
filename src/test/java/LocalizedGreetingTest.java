60733fce91ac9424e3dbcc260cd09a2046acb9a4
Mihuy1 <patrik.hyytiainen@protonmail.com>
2024-10-21 13:36:35 +0300

    refractor

--- src/test/java/LocalizedGreetingTest.java
+++ src/test/java/LocalizedGreetingTest.java
@@ -1,4 +1,42 @@
+import org.junit.jupiter.api.Test;
+import java.util.Locale;
+import java.util.ResourceBundle;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class LocalizedGreetingTest {
-  
+
+    @Test
+    public void testEnglishGreeting() {
+        Locale locale = new Locale("en", "US");
+        ResourceBundle messages = ResourceBundle.getBundle("MessagesBundle", locale);
+        String greeting = messages.getString("greeting");
+        assertEquals("Hello! Welcome to our application.", greeting);
+    }
+
+    @Test
+    public void testSpanishGreeting() {
+        Locale locale = new Locale("es", "ES");
+        ResourceBundle messages = ResourceBundle.getBundle("MessagesBundle", locale);
+        String greeting = messages.getString("greeting");
+        assertEquals("¡Hola! Bienvenido a nuestra aplicacion.", greeting);
+    }
+
+    @Test
+    public void testFrenchGreeting() {
+        Locale locale = new Locale("fr", "FR");
+        ResourceBundle messages = ResourceBundle.getBundle("MessagesBundle", locale);
+        String greeting = messages.getString("greeting");
+        assertEquals("Bonjour! Bienvenue dans notre application.", greeting);
+    }
+
+    @Test
+    public void testInvalidChoiceDefaultsToEnglish() {
+        Locale locale = new Locale("en", "US"); // Simulate default choice
+        ResourceBundle messages = ResourceBundle.getBundle("MessagesBundle", locale);
+        String greeting = messages.getString("greeting");
+        assertEquals("Hello! Welcome to our application.", greeting); // Expected default is English
+    }
+    
+
 }
